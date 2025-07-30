e90e4c3a9129a120300c6235a291170bcc2db47f
= <=>
2025-07-30 13:15:27 +0200

    30.07.2025 Conexión con MQTT

---
+++ 36_mqtt_basico/src/main.cpp
@@ -0,0 +1,91 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char *ssid = "Aula 1";
+const char *pwd = "Horus.2025";
+
+//* Conexion con MQTT
+const char *mqtt_server = "192.168.0.25";
+int mqtt_port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+const char *mqtt_topic_sub = "curso_arduino/#";
+const char *mqtt_topic_pub = "curso_arduino/alfonso";
+const char *mqtt_client_id = "ESP_Alfonso";
+
+void callback(char *topic, byte *pyload, unsigned int length);
+void reconnect();
+
+static unsigned long lastMsg = 0;
+unsigned long now;
+
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.begin(ssid, pwd);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("\n");
+  Serial.print("Conectado a: ");
+  Serial.println(WiFi.localIP());
+
+  // MQTT
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+
+void loop()
+{
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
+
+  now = millis();
+  if (now - lastMsg > 20000)
+  {
+    lastMsg = now;
+    String msg = "El mensaje de Alfonso " + String(now);
+    client.publish(mqtt_topic_pub, msg.c_str());
+    Serial.println("Mensaje enviado: " + msg);
+  }
+}
+
+// Functions
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Mensaje recibido en [");
+  Serial.print(topic);
+  Serial.print("]: ");
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect()
+{
+  while (!client.connected())
+  {
+    Serial.println("Reconectando ....");
+    if (client.connect(mqtt_client_id))
+    {
+      Serial.println("Conectado a MQTT.");
+      client.subscribe(mqtt_topic_sub);
+    }
+    else
+    {
+      Serial.println("Fallo en la conexión");
+      Serial.println("Error : " + client.state());
+      delay(5000);
+    }
+  }
+}
